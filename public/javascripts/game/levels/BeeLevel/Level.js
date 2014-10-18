22cbb26232c49285d38992a68849bb6bd2e1c981
Ben Myers <bmyers@appnexus.com>
2014-10-18 02:04:19 -0400

    Moving level maps outside of level-config since that would have gotten bloated fast

--- public/javascripts/game/levels/BeeLevel/Level.js
+++ public/javascripts/game/levels/BeeLevel/Level.js
@@ -4,29 +4,21 @@ define([
 	'game/levels/BeeLevel/Log',
 	'game/base/Widget',
 	'game/base/TileGrid',
-	'game/config/level-config'
+	'game/levels/BeeLevel/map',
 ], function(
 	Bee,
 	Log,
 	Widget,
 	TileGrid,
-	levelConfig
+	map
 ) {
 	function BeeLevel() {
 		this.backgroundColor = '#222';
 		this.player = new Bee(this);
 		this.player.pos.x = 300;
 		this.player.pos.y = 415;
-		this.backgroundTileGrid = new TileGrid(
-			levelConfig.LEVEL_1.background.tiles,
-			levelConfig.LEVEL_1.background.shapes,
-			levelConfig.LEVEL_1.background.variants
-		);
-		this.tileGrid = new TileGrid(
-			levelConfig.LEVEL_1.foreground.tiles,
-			levelConfig.LEVEL_1.foreground.shapes,
-			levelConfig.LEVEL_1.foreground.variants
-		);
+		this.backgroundTileGrid = new TileGrid(map.background.tiles,map.background.shapes,map.background.variants);
+		this.tileGrid = new TileGrid(map.foreground.tiles,map.foreground.shapes,map.foreground.variants);
 		this.obstacles = [ new Log(600, 457) ];
 		this.actors = [ new Bee(this) ];
 		this.actors[0].pos.x = 400;
